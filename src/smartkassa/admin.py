9e4061ae4efa906e0dcc1497eee82ca6dafe46f0
aziz <sharifboev.7336@gmail.com>
2025-03-20 17:49:12 +0500

    Добавлены фильтры, поиск и inline-устройства в Django Admin

--- src/smartkassa/admin.py
+++ src/smartkassa/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-
 from .models import Client, Device
-
 
 class SmartKassaAdminSite(admin.AdminSite):
     site_header = "Smart Kassa"
@@ -13,16 +11,32 @@ class SmartKassaAdminSite(admin.AdminSite):
         context["logo"] = "/static/logo.png"
         return context
 
+class DeviceInline(admin.StackedInline):  # Inline для устройств внутри клиента
+    model = Device
+    extra = 1
+
+    def save_model(self, request, obj, form, change):
+        if not obj.client:
+            obj.client = form.instance  # Автоматически проставляем клиента
+        super().save_model(request, obj, form, change)
+
 
 class ClientAdmin(admin.ModelAdmin):
     search_fields = ("inn", "name", "pinfl", "phone")
+    inlines = [DeviceInline]  # Встраиваем устройства внутрь клиента
 
     class Media:
         js = ("admin/js/client_auto_fill.js",)
 
 
+
 class DeviceAdmin(admin.ModelAdmin):
-    pass
+    def has_add_permission(self, request):
+        return False
+
+    list_filter = ["is_active", "owner_type"]
+    search_fields = ["name", "kkm_serial_number", "fm_serial_number"]
+
 
 
 admin_site = SmartKassaAdminSite(name="smartkassa")
